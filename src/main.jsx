d2b80ca70b8abdd1377be4a5c5ac6cac936c529c
abujaher5 <abujaher5120@gmail.com>
2024-11-20 00:55:16 +0600

    added navbar.jsx component and basic setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes/Routes.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <div className="max-w-7xl mx-auto">
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>
+  </div>
+);
