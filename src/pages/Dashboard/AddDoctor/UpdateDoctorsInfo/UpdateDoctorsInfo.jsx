90dbaa2f73757d4693e2549ef0477a88ec4ee915
abujaher5 <abujaher5120@gmail.com>
2025-09-01 15:04:56 +0600

    implement updateDoctorsInfo button successfully

--- src/pages/Dashboard/AddDoctor/UpdateDoctorsInfo/UpdateDoctorsInfo.jsx
+++ src/pages/Dashboard/AddDoctor/UpdateDoctorsInfo/UpdateDoctorsInfo.jsx
@@ -1,18 +1,61 @@
 import { useLoaderData, useNavigate } from "react-router-dom";
 import useAxiosSecure from "../../../../hooks/useAxiosSecure";
 import { useForm } from "react-hook-form";
+import useAxiosPublic from "../../../../hooks/useAxiosPublic";
+import Swal from "sweetalert2";
+const image_hosting_key = import.meta.env.VITE_image_hosting_key;
+const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`;
 
 const UpdateDoctorsInfo = () => {
   const { name, specialize, location, availableOn, availableTime, image, _id } =
     useLoaderData();
   const { register, handleSubmit, reset } = useForm();
   const axiosSecure = useAxiosSecure();
+  const axiosPublic = useAxiosPublic();
   const navigate = useNavigate();
   console.log(name, _id);
   const onSubmit = async (data) => {
     console.log(data);
-  };
+    console.log("Button Clicked");
 
+    const imageFile = { image: data.image[0] };
+    const res = await axiosPublic.post(image_hosting_api, imageFile, {
+      headers: {
+        "content-type": "multipart/form-data",
+      },
+    });
+    console.log(res);
+
+    if (res.data.success) {
+      const updatedItem = {
+        name: data.name,
+        specialize: data.specialize,
+        location: data.location,
+        availableOn: data.availableOn,
+        availableTime: data.availableTime,
+        image: res.data.data.display_url,
+      };
+
+      const updatedRes = await axiosSecure.patch(
+        `/doctors/${_id}`,
+        updatedItem
+      );
+
+      console.log(updatedRes.data);
+
+      if (updatedRes.data.modifiedCount > 0) {
+        reset();
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: `${data.name} information updated Successfully.`,
+          showConfirmButton: false,
+          timer: 1500,
+        });
+        navigate("/dashboard/manageDoctors");
+      }
+    }
+  };
   return (
     <div>
       <div className="mt-6 bg-cyan-700 p-10 rounded-xl shadow-lg text-white ">
@@ -125,7 +168,6 @@ const UpdateDoctorsInfo = () => {
               <input
                 {...register("image", { required: true })}
                 type="file"
-                // defaultValue={image}
                 className="file-input text-black 
                 text-opacity-40
                 file-input-bordered w-full"
